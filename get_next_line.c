09e57df37c761cb037638bb25ef7c6db9dde1a08
ked-source <karim.diab@alpheya.com>
2026-03-01 13:16:20 +0400

    added function prototypes

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,4 @@
-
-
+#include "get_next_line.h"
 
 char *get_next_line(int fd)
 {
---
+++ get_next_line.h
@@ -0,0 +1,12 @@
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 42
+
+char *get_next_line(int fd);
+int ft_strlen(char *str);
+
+#endif
